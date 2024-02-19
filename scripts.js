adfe324ab5d172f845e7a836d1d9d565fdefaee6
heena25 <parveenheena196@gmail.com>
2024-02-19 16:11:44 +0530

    src file added

---
+++ scripts.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const logoutBtn = document.getElementById('logoutBtn');
+    logoutBtn.addEventListener('click',()=>{
+        logout();
+    })
+
+    function logout(){
+        fetch('')
+    }
+})
